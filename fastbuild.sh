3e7631a7828b1938540a70c5002729ff5b0cc0c8
CodexDive <145115737+CodexDive@users.noreply.github.com>
2023-12-04 16:54:02 +0800

    Update fastbuild.sh

--- fastbuild.sh
+++ fastbuild.sh
@@ -12,31 +12,20 @@ function show_help {
 }
 
 
-work_dir="/home/mzw/FB_opensource"
-frontend_dir="$work_dir/frontend"
-backend_dir="$work_dir/backend"
+work_dir=$(pwd)
+frontend_dir="$work_dir/Web"
+backend_dir="$work_dir/Backend"
 nginx_dir="$work_dir/nginx"
-
-github_frontend="https://github.com/CodexDive/FastBuild-Web.git"
-github_backend="https://github.com/CodexDive/FastBuild-Backend.git"
-fastbuild_version="release-1.0"
+nginx_setup="$nginx_dir/setup"
 
 port="48001"
 
 
 
 function install_main {
-    #fetch_fastbuild_code
     check_environment
     install_env_requirement
     start_services
-}
-
-function fetch_fastbuild_code {
-    cd $frontend_dir
-    git clone -b $fastbuild_version $github_frontend
-    cd $backend_dir
-    git clone -b $fastbuild_version $github_backend
 }
 
 
@@ -67,10 +56,9 @@ function install_env_requirement {
 function start_services {
     # 启动后端程序
     cd $backend_dir
-    nohup python main.py > backend.log 2>&1 &
-
+    nohup python $backend_dir/main.py > Fastbuild_Backend.log 2>&1 &=
     # 启动Nginx前端服务
-    sudo ... ... ... ... /usr/local/nginx/sbin/nginx
+    $nginx_dir/sbin/nginx -s reload
 }
 
 
@@ -111,11 +99,11 @@ function install_nginx_dependents {
 
 
 function load_install_nginx {
-    cd $nginx_dir
+    cd $nginx_setup
     wget "http://nginx.org/download/nginx-1.21.5.tar.gz"
     tar -zxvf nginx-1.21.5.tar.gz
     cd nginx-1.21.5
-    ./configure --prefix=$nginx_dir
+    ./configure --prefix=$nginx_dir --user=fastbuild --group=fastbuild --with-http_ssl_module --with-http_stub_status_module --with-http_gzip_static_module --with-pcre
     make
     sudo make install
 }
@@ -123,23 +111,23 @@ function load_install_nginx {
 
 function configure_nginx {
     # 修改Nginx配置文件
-    echo "server {
-        listen $port;
-        server_name localhost;
+    sed -i "$i\
+    server {\
+        listen $port;\
+        server_name localhost;\
+        \
+        location / {\
+            root $frontend_dir/dist;\
+            index index.html;\
+        }\
+        location /api {\
+            proxy_pass  http://127.0.0.1:40002;\
+        }\
+    }" $nginx_dir/conf/nginx.conf
 
-        location / {
-            root $frontend_dir/dist;
-            index index.html;
-        }
-    }" | sudo tee -a $nginx_dir/conf/nginx.conf
-
-    # 创建符号链接
-    nginx_sites_available="/etc/nginx/sites-available"
-    nginx_sites_enabled="/etc/nginx/sites-enabled"
-    ln -s $nginx_sites_available/fastbuild_frontend $nginx_sites_enabled/
 
     # 检查Nginx配置文件语法
-    nginx -t
+    $nginx_dir/sbin/nginx -t
 }
 
 
@@ -150,68 +138,15 @@ function install_backend_requirement {
 }
 
 
-# 安装函数
-function install {
-    # 拉取前端代码
-    git clone $github_frontend
-    cd FastBuild-Web
-    # 构建前端代码
-    # 这里假设您的前端代码已经构建好了，如果需要构建，请添加构建步骤
-
-    
-
-    # 部署前端代码到nginx
-    sudo cp -r dist/* /usr/local/nginx/html/
-
-    # 拉取后端代码
-    cd ..
-    git clone <backend_repository_url>
-    cd backend
-
-    # 检查端口是否被占用
-    if lsof -Pi :8000 -sTCP:LISTEN -t >/dev/null; then
-        echo "Port 8000 is already in use. Please make sure it's available before running the backend application."
-        exit 1
-    fi
-
-    # 安装后端依赖
-    pip install -r requirements.txt
-
-    # 启动后端程序
-    nohup python main.py > backend.log 2>&1 &
-
-    # 前端代码路径
-    frontend_path="/home/fastbuild/web/frontend"
-    frontend_repository_url="<frontend_repository_url>"
-
-
-
-    # 重启Nginx
-    systemctl restart nginx
-
-    # 启动Nginx
-    sudo /usr/local/nginx/sbin/nginx
-
-}
-
 # 卸载函数
 function uninstall {
     # 停止后端程序
-    pkill -f "python main.py"
-
-    # 停止nginx
-    sudo systemctl stop nginx
-
-    # 移除前端代码
-    sudo rm -rf /var/www/html/*
-
-    # 移除后端代码
-    cd ..
-    sudo rm -rf backend
+    pkill -f "python $backend_dir/main.py"
 
     # 移除nginx
-    sudo apt purge nginx -y
+    sudo rm -rf $nginx_dir
 }
+
 
 # 处理命令行参数
 case "$1" in
